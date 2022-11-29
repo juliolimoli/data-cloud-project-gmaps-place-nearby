33c847e5dceb40ad5f9272b71e72596baafcf042
Júlio Limoli <juliolimolisilva@gmail.com>
2022-11-29 20:33:16 -0300

    Adding virtual environment and some changes on the main scrti

--- project/main.py
+++ project/main.py
@@ -1,12 +1,15 @@
 # libraries imports here
+import requests as req
 import boto3
 from botocore.exceptions import ClientError
 
 # get secret from secret manager service
 def get_secret():
+    """Function that get secrets from the AWS Secret Manager Service"""
 
     #environment = event['env'] # get environment variable
-    secret_name ='%s/GMapsAPI' % environment
+    # should setup env variable
+    secret_name = f'{environment}/GMapsAPI'
     region_name = 'sa-east-1'
 
     # Create a Secrets Manager client
@@ -22,11 +25,14 @@ def get_secret():
         )
     except ClientError as e:
         # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_
+        # GetSecretValue.html
         raise e
 
     # Decrypts secret using the associated KMS key.
     secret = get_secret_value_response['SecretString']
+
+    return secret
 
 
 # function that retry and abort on attempts to do something
@@ -39,4 +45,14 @@ def retry_abort(max_retries=3):
 
 # function that queries the location used in the nearby search
 def query_lat_lon():
+    """Function that makes the requests for the Maps API - Nearby Search. 
+    
+    Parameters:
+    lat (str): Latitude value
+    lon (str): Longitude value
+    
+    Returns:
+    JSON: Response of the request
+    """
+    secret = get_secret()
     pass
