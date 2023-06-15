5a6c230069f86f887eedc89ec67c04bb06f573ab
juliolimoli <juliolimolisilva@gmail.com>
2023-06-15 13:13:08 +0200

    Refactoring after change for Scheduler client.

--- app.py
+++ app.py
@@ -7,24 +7,6 @@ import time
 from datetime import datetime
 import json
 
-# function that retry and abort on attempts to do something
-def retry_abort(func: function, max_retries: int = 3):
-    def retry_abort_wrapper(*args, **kwargs):
-        function_name = func.__name__
-        print(f"Initializing the function: {function_name}")
-        for attempt in range(1, max_retries+1):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                print(e)
-                if attempt == max_retries:
-                    # abort
-                    raise e
-                time.sleep(5)
-    return retry_abort_wrapper
-
-# function that requests in the nearby search
-@retry_abort
 def nearby_search(
     lat: str = None,
     lon: str = None,
@@ -53,7 +35,7 @@ def nearby_search(
     endpoint = 'https://maps.googleapis.com/maps/api/place/nearbysearch/'
     payload={}
     headers = {}
-    #####
+
     if next_page_token is None:
         # request the API
         url_loc = f"{endpoint}{response_format}?location={lat}%2c{lon}&radius=\
@@ -62,14 +44,12 @@ def nearby_search(
         return response
     else:
         time.sleep(4)
-        token = next_page_token
-        url_loc = f"{endpoint}{response_format}?pagetoken={token}\
+        url_loc = f"{endpoint}{response_format}?pagetoken={next_page_token}\
 &key={API_KEY}"
         response = req.request("GET", url_loc, headers=headers, data=payload)
         return response
 
 # function that saves data in S3 bucket
-@retry_abort
 def s3_put_object(
     bucket_name: str, 
     file_key: str,
@@ -85,15 +65,15 @@ def s3_put_object(
         True if file was uploaded, else False
     """
     # File to upload
-    upload_byte_stream = bytes(file.encode("UTF-8"))
-
+    #json.dumps(file, ensure_ascii=False).encode('utf8')
     # Upload the file
     s3_client = boto3.client("s3")
     try:
         response = s3_client.put_object(
             Bucket=bucket_name, 
             Key=file_key, 
-            Body=upload_byte_stream
+            Body=file,
+            ContentType="text/plain;charset=utf-8"
             )
     except ClientError as e:
         print(e)
@@ -101,33 +81,37 @@ def s3_put_object(
     return True
 
 def send_to_details_lambda(results, context):
-    
+    region = "sa-east-1"
+    acc_id = "820949372807"
+    event_bus_name = f"arn:aws:events:{region}:{acc_id}:event-bus/default"
+    print("sending details")
     # Get all Place Id's
     places_ids = [place['place_id'] for place in results]
     event_to_details = {'places_ids': places_ids}
-
+    print(event_to_details)
     # send the events to EventBridge and then to the nearby lambda
     event_bridge_client = boto3.client('events')
-
     # Define the parameters for the PutEvents operation
     put_events_params = {
-        'Entries': [
-                {
-                'Source': context.function_name,
-                'Target': "data-cloud-project-gmaps-details",
-                'Detail': json.dumps(event_to_details)
-            }
+        "Entries": [
+            {
+            "Source": context.function_name,
+            "DetailType": "Nearby to Details",
+            "Detail": json.dumps(event_to_details),
+            "EventBusName": event_bus_name
+        }
         ]
     }
+    print(put_events_params)
     # Send the event to EventBridge
     response = event_bridge_client.put_events(**put_events_params)
     print(response)
 
 # lambda_handler function
 def lambda_handler(event, context):
-
+    print(event)
     # Define the initial variables
-    coordinate = (event["latitude"], event["longitude"])
+    coordinate = (event["coordinate"][0], event["coordinate"][1])
     radius = event["radius"]
 
     # requesting the Nearby Places API
@@ -137,12 +121,14 @@ def lambda_handler(event, context):
         radius=radius
         )
     response_dict = json.loads(response.text)
-    
+    response_encoded = json.dumps(
+        response_dict, 
+        ensure_ascii=False
+        ).encode('utf8')  
     # Upload to S3
-    t = datetime.now()
-    timestamp = datetime.strftime(t, "%Y%m%d%H%M%S%f")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S%f")
     coordinate_string = str(coordinate[0])+"_"+str(coordinate[1])
-    bucket = "SoR"
+    bucket = "dcpgm-sor"
     prefix = "gmaps/nearby/"
     file_name = f"{coordinate_string}_{timestamp}_1.json"
     key = f"{prefix}{file_name}"
@@ -150,32 +136,34 @@ def lambda_handler(event, context):
     s3_put_object(
         bucket_name=bucket,
         file_key=key,
-        file=response_dict
+        file=response_encoded
         )
     
-    # Create details lambda event in EventBridge
     send_to_details_lambda(response_dict['results'], context)
 
     # pagination handler
-    for page in range(2,4):
+    for page in [2,3]:
         if "next_page_token" in response_dict:
+            print("token", page)
             token = response_dict["next_page_token"]
             response = nearby_search(next_page_token=token)
             response_dict = json.loads(response.text)
-
+            response_encoded = json.dumps(
+                response_dict, 
+                ensure_ascii=False
+                ).encode('utf8')
             # Upload to S3
-            t = datetime.now()
-            timestamp = datetime.strftime(t, "%Y%m%d%H%M%S%f")
             file_name = f"{coordinate_string}_{timestamp}_{page}.json"
             key = f"{prefix}{file_name}"
 
             s3_put_object(
                 bucket_name=bucket,
                 file_key=key,
-                file=response_dict
+                file=response_encoded
                 )
 
             # Create details lambda event in EventBridge
+            print("pre-sending")
             send_to_details_lambda(response_dict['results'], context)
         else:
             print("Sem token")
